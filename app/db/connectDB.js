1f6ca28563f2025e7277dd16fc6b4e79d7abe21c
krishna3032004 <krishnagupta30mar@gmail.com>
2025-02-05 17:07:10 +0530

    change

--- app/db/connectDB.js
+++ app/db/connectDB.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`${process.env.DATABASE_URL}`);
+    const conn = await mongoose.connect(`${process.env.DATABASE_URL}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  });
     console.log(`MongoDB Connected: {conn.connection.host}`);
   } catch (error) {
     console.error(error.message);
