f3e5cb78c2a518bb8ba9e168652bdae9717b3689
krishna3032004 <krishnagupta30mar@gmail.com>
2025-02-06 12:12:22 +0530

    change

--- app/components/ScrollHandler.js
+++ app/components/ScrollHandler.js
@@ -4,11 +4,13 @@ import { useEffect } from "react";
 import { usePathname } from "next/navigation"; // Works for both App Router and Pages Router
 
 const ScrollHandler = () => {
-    const pathname = usePathname(); // Get the current page route
+    const pathname = usePathname();
+    console.log(pathname) // Get the current page route
 
     useEffect(() => {
         if (typeof window !== "undefined") {
             window.scrollTo(0, 0); // Force scroll to top when the route changes
+            console.log("scroll to top")
         }
     }, [pathname]); // Runs whenever the route changes
 
